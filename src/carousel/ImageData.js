6b6da2caaf2ab17a52ae80622eca62d67c9e1de0
Rupali-app-hub <rupalipriyadarsini.atwork@gmail.com>
2024-01-12 21:37:24 +0530

    image removed

--- src/carousel/ImageData.js
+++ src/carousel/ImageData.js
@@ -1,6 +1,5 @@
 export const ImageData =[
     {id:1, image:require('../images/Image1.jpeg')},
-    {id:2, image:require('../images/Image2.png')},
     {id:3, image:require('../images/Image3.jpeg')},
     {id:4, image:require('../images/Image4.jpeg')},
     {id:5, image:require('../images/Image5.jpeg')},
